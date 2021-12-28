25fe7ca7a5baf6a4ae40521e726116578efe13b3
Alex Loskutov <599181+aloskutov@users.noreply.github.com>
2021-12-28 16:42:12 +0700

    added some test

--- test/externalLinksTest.js
+++ test/externalLinksTest.js
@@ -21,7 +21,7 @@ test('outputPath is js', (t) => {
   t.is(externalLinks(content, outputPath), content);
 });
 
-test('local links check', (t) => {
+test('local links, relative', (t) => {
   const outputPath = 'test.html';
   const content = '<!doctype html><html><head></head><body>' +
   '<p>Lorem ipsum dolor sit <a href="/local-link">amet</a>, consectetur adipiscing elit. Phasellus ac nunc ut enim lobortis dignissim. Praesent nisl dui, venenatis a tortor sit amet, bibendum vestibulum mauris. Sed sed pulvinar nunc. Morbi quis efficitur ipsum. <a href="/another-link.html">Proin vestibulum eleifend</a> sem eu sodales. Phasellus id est vel eros commodo placerat eu consectetur arcu. Vestibulum egestas mauris non aliquet varius.</p>' +
@@ -29,7 +29,23 @@ test('local links check', (t) => {
   t.is(externalLinks(content, outputPath), content);
 });
 
-test('Local links', (t) => {
+test('local links with query string', (t) => {
+  const outputPath = 'test.html';
+  const content = '<!doctype html><html><head></head><body>' +
+  '<p>Lorem ipsum dolor sit <a href="/what-is/?amet">amet</a>, consectetur adipiscing elit. Phasellus ac nunc ut enim lobortis dignissim. Praesent nisl dui, venenatis a tortor sit amet, bibendum vestibulum mauris. Sed sed pulvinar nunc. Morbi quis efficitur ipsum. <a href="?another-link=123">Proin vestibulum eleifend</a> sem eu sodales. Phasellus id est vel eros commodo placerat eu consectetur arcu. Vestibulum egestas mauris non aliquet varius.</p>' +
+  '</body></html>';
+  t.is(externalLinks(content, outputPath), content);
+});
+
+test('local links with id', (t) => {
+  const outputPath = 'test.html';
+  const content = '<!doctype html><html><head></head><body>' +
+  '<p>Lorem ipsum dolor sit <a href="#amet">amet</a>, consectetur adipiscing elit. Phasellus ac nunc ut enim lobortis dignissim. Praesent nisl dui, venenatis a tortor sit amet, bibendum vestibulum mauris. Sed sed pulvinar nunc. Morbi quis efficitur ipsum. <a href="/another-link/#123">Proin vestibulum eleifend</a> sem eu sodales. Phasellus id est vel eros commodo placerat eu consectetur arcu. Vestibulum egestas mauris non aliquet varius.</p>' +
+  '</body></html>';
+  t.is(externalLinks(content, outputPath), content);
+});
+
+test('Local links, with domain', (t) => {
   const outputPath = 'test.html';
   const config = {'url': 'www.example.com'};
   const content = '<!doctype html><html><head></head><body>' +
