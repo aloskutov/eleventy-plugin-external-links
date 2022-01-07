1e1761a1b6256f57bfb1252c8f8c18ec321f3aaa
Alex Loskutov <599181+aloskutov@users.noreply.github.com>
2022-01-07 14:58:37 +0700

    removed options

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,6 @@
 const externalLinks = require('./src/externalLinks');
 
 module.exports = (eleventyConfig, options = {}) => {
-  options = Object.assign(
-    {
-      url: null,
-      selector: 'a',
-      rel: ['noreferrer', 'nofollow', 'noopener', 'external'],
-      target: '_blank',
-      overwrite: true,
-    },
-    options
-  );
 
   eleventyConfig.addTransform('externalLinks', (content, outputPath) => {
     return externalLinks(content, outputPath, options);
