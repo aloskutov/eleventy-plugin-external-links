dab373c7417746a2392f576b1f12e9e0105f63ae
Alex Loskutov <599181+aloskutov@users.noreply.github.com>
2022-01-10 12:52:20 +0700

    fix: JS-D007

--- src/getHostname.js
+++ src/getHostname.js
@@ -9,8 +9,9 @@
 function parseURL(url) {
   const checkedUrl = (url !== null) ? url.toLowerCase() : '';
   const regex = /^(?<protocol>\w*(?::))?(?:\/\/)?(?<hostname>[a-zA-Z0-9.-]+)?(?::)?(?<port>\d+)?(?<path>\/?[\w.]*\/?)*(?<query>\S*)/;
+  const matches = regex.exec(checkedUrl);
 
-  return regex.exec(checkedUrl);
+  return matches;
 }
 
 /**
