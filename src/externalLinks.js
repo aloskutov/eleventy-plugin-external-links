f40275ba499e23e998bf16582d8a966ca26c68ae
Alex Loskutov <599181+aloskutov@users.noreply.github.com>
2022-01-16 19:02:09 +0700

    link checking refactoring

--- src/externalLinks.js
+++ src/externalLinks.js
@@ -1,10 +1,12 @@
 'use strict';
 
 const {JSDOM} = require('jsdom');
-const getHostname = require('./getHostname');
+const path = require('path');
+
 const parseOptions = require('./parseOptions');
 const getExcludedHosts = require('./getExcludedHosts');
-const path = require('path');
+const getHostname = require('./getHostname');
+
 const defaultOptions = {
   url: '',
   selector: 'a',
@@ -18,6 +20,25 @@ const defaultOptions = {
   excludedDomains: [],
 };
 
+
+/**
+ * Should Change Attributes
+ * @param {string} link
+ * @param {object} options
+ * @return {boolean}
+ */
+const shouldChangeAttributes = (link, options) => {
+  const hostname = getHostname(options.url);
+  const linkHostname = getHostname(link, options.excludedProtocols);
+  const excludedHosts = getExcludedHosts(options.excludedDomains);
+
+  return (
+    linkHostname &&
+    linkHostname !== hostname &&
+    !excludedHosts.includes(linkHostname)
+  );
+};
+
 module.exports = function(content, outputPath, globalOptions = {}) {
   if (!outputPath) return content;
 
@@ -25,23 +46,17 @@ module.exports = function(content, outputPath, globalOptions = {}) {
 
   if (!options.ext.includes(path.extname(outputPath))) return content;
 
-  const hostname = getHostname(options.url);
-  const excludedHosts = getExcludedHosts(options.excludedDomains);
   const dom = new JSDOM(content);
   const document = Object.assign(dom.window.document);
   const [...links] = document.querySelectorAll(options.selector);
 
   links.forEach((link) => {
-    const linkHostname = getHostname(link.getAttribute('href'), options.excludedProtocols);
+    const linkHref = link.getAttribute('href');
     const linkRel = link.getAttribute('rel');
     const linkTarget = link.getAttribute('target');
     const rel = Array.isArray(options.rel) ? options.rel.join(' ') : options.rel;
 
-    if (
-      linkHostname &&
-      linkHostname !== hostname &&
-      !excludedHosts.includes(linkHostname)
-    ) {
+    if (shouldChangeAttributes(linkHref, options)) {
       if (options.overwrite) {
         link.setAttribute('rel', rel);
         link.setAttribute('target', options.target);
