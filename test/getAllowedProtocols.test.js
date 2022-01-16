d71e63ef5b1e3c47b32d9650f7ca4e6693dd6142
Alex Loskutov <599181+aloskutov@users.noreply.github.com>
2022-01-16 16:05:06 +0700

    fixed

--- test/getAllowedProtocols.test.js
+++ test/getAllowedProtocols.test.js
@@ -1,14 +1,15 @@
 'use strict';
 
-const getAllowedProtocols = require('../src/getAllowedProtocols');
-const allowedProcotols = ['http', 'https', 'ftp', 'ftps', null];
+const getAllowedProtocols= require('../src/getAllowedProtocols');
+
+const allowedProtocols = ['http', 'https', 'ftp', 'ftps', null];
 
 describe('Allowed procols', () => {
   it('Test #1', () => {
-    expect(getAllowedProtocols()).toEqual(allowedProcotols);
-    expect(getAllowedProtocols([])).toEqual(allowedProcotols);
+    expect(getAllowedProtocols()).toEqual(allowedProtocols);
+    expect(getAllowedProtocols([])).toEqual(allowedProtocols);
     expect(getAllowedProtocols(null)).toEqual(['http', 'https', 'ftp', 'ftps']);
-    expect(getAllowedProtocols(allowedProcotols)).toEqual([]);
+    expect(getAllowedProtocols(allowedProtocols)).toEqual([]);
     expect(getAllowedProtocols('FTP')).not.toContain('ftp');
     expect(getAllowedProtocols(['Ftp', 'ftps'])).not.toContain(['ftp', 'ftps']);
   });
