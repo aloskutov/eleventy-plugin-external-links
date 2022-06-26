b79a31c90b68c16eea931757dcb7f016e03b950c
Alex Loskutov <599181+aloskutov@users.noreply.github.com>
2022-06-26 15:26:04 +0700

    pattern update

--- src/parseURL.js
+++ src/parseURL.js
@@ -8,7 +8,7 @@
  */
 const parseURL = (url) =>{
   const checkedUrl = url ? url.toLowerCase() : '';
-  const regex = /^(?<protocol>\w{0,25}(?::)){0,1}(?:\/\/){0,1}(?:(?:\S{0,255}){0,1}(?:@)){0,1}(?<hostname>[\p{L}\p{N}][\p{L}\p{N}.-]{0,252}[\p{L}\p{N}]){0,1}/u;
+  const regex = /^(?<protocol>[\w][\w\d.+-]{0,128}(?::)){0,1}(?:\/\/){0,1}(?:(?<userInfo>[\w\d!$&'()*+,;=:]{1,127})@){0,1}(?<hostname>(\[[0-9a-f]{0,4}(?::[[0-9a-f]{0,4}){1,7}\])|(?:[0-9]{1,3}\.){3}[0-9]{1,3}|[\p{L}\p{N}][\p{L}\p{N}.-]{0,252}[\p{L}\p{N}]){0,1}/u;
   const matches = regex.exec(checkedUrl);
 
   return matches;
